fdb464384c89d780874847f0f8cb361a09934cb9
JaewoongLee-swift <jaewoong0624@gmail.com>
2021-10-13 18:28:35 +0900

    Week 9, 날씨앱 api 에러메세지 alert 구현

---
+++ Week 9/Weather/Weather/ErrorMessage.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorMessage.swift
+//  Weather
+//
+//  Created by 이재웅 on 2021/10/13.
+//
+
+import Foundation
+
+// 에러 메세지를 전달하기 위한 구조체
+struct ErrorMessage: Codable {
+    let message: String
+}
--- Week 9/Weather/Weather/ViewController.swift
+++ Week 9/Weather/Weather/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var maxTempLabel: UILabel!
     @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var weatherStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,17 +27,46 @@ class ViewController: UIViewController {
         }
     }
     
+    func configureView(weatherInformation: WeatherInformation) {
+        self.cityNameLabel.text = weatherInformation.name
+        if let weather = weatherInformation.weather.first {
+            self.weatherDescriptionLabel.text = weather.description
+        }
+        self.tempLabel.text = "\(Int(weatherInformation.temp.temp - 273.15))"
+        self.minTempLabel.text = "최저: \(Int(weatherInformation.temp.minTemp - 273.15))℃"
+        self.maxTempLabel.text = "최고: \(Int(weatherInformation.temp.maxTemp - 273.15))℃"
+    }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "에러", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     func getCurrentWeather(cityName: String) {
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=010e6e80ce8579ce8dfb9a912f307924") else { return }
         let session = URLSession(configuration: .default)
-        session.dataTask(with: url) { data, response, error in
+        session.dataTask(with: url) { [weak self] data, response, error in
             // data : 서버에서 응답받은 데이터(우리가 받을 json)
             // response : http 헤더 및 상태코드와 같은 응답 미터 데이터
             // error : 요청을 실패하게 되면 에러객체가 전달됨. 요청에 성공하면 nil이 반환
+            let successRange = (200..<300)                      // api 요청성공은 statusCode가 200번대일 때 성공
             guard let data = data, error == nil else { return } // error == nil 이면 요청 성공
             let decoder = JSONDecoder()
-            let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data)
-            debugPrint(weatherInformation)
+            if let response = response as? HTTPURLResponse, successRange.contains(response.statusCode) {
+                guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else { return }
+                
+                DispatchQueue.main.async {
+                    self?.weatherStackView.isHidden = false
+                    self?.configureView(weatherInformation: weatherInformation)
+                }
+            } else {
+                guard let errorMessage = try? decoder.decode(ErrorMessage.self, from: data) else { return }
+                DispatchQueue.main.async {
+                    self?.showAlert(message: errorMessage.message)
+                }
+            }
+            
         }.resume()
     }
     
