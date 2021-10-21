1a43f606291a79636defc375a571ddde15cb4fc3
JaewoongLee-swift <jaewoong0624@gmail.com>
2021-10-21 17:42:37 +0900

    Week 10, 자산관리 앱 TabBar 구현

---
+++ Week 10/MyAssets/MyAssets/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  MyAssets
+//
+//  Created by 이재웅 on 2021/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection: Tab = .asset
+    
+    enum Tab {
+        case asset
+        case recommend
+        case alert
+        case setting
+    }
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            Color.white
+                .tabItem {
+                    Image(systemName: "dollarsign.circle.fill")
+                    Text("자산")
+                }
+                .tag(Tab.asset)
+            Color.blue
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "hand.thumbsup.fill")
+                    Text("추천")
+                }
+                .tag(Tab.recommend)
+            Color.yellow
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "bell.fill")
+                    Text("알림")
+                }
+                .tag(Tab.alert)
+            Color.red
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text("설정")
+                }
+                .tag(Tab.setting)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Week 10/MyAssets/MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by 이재웅 on 2021/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
